65b691af4406500b3684c446484fb8182b96a5a6
fiveft55 <arpanx980@gmail.com>
2025-07-12 04:58:18 +1000

    update for test

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello world!")
+# + ---------------------------------------------------- +
+# |                                                      |
+# |     This is a simple video downloader prototype      |
+# |                                                      |
+# + ---------------------------------------------------- +
+
+# import .config
+import sys
+import json
+import yt_dlp
+
+# demo main()
+def main():
+    print("Welcome to lucky Devil!")
+    
+    name = input("What is your name?: ")
+    print(f"Hello, {name}! Nice to meet you...")
+
+    while True:
+        choice = input("Enter 'quit' to exit, or anything else to continue: ")
+        if choice.lower() == 'quit' or choice.lower() =='exit':
+            break
+        print("You chose to continue.")
+
+    print("Exiting the application. Goodbye!")
+
+
+# sys.req-2-exc
+if __name__ == "__main__":
+    main()
