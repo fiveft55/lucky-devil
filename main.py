aab44509498bfa4370247c1fd6e65da62f951306
fiveft55 <arpanx980@gmail.com>
2025-07-21 00:21:28 +1000

    Test phase for most sites are under progress and have solved the possible issue of downloading video and audio in best format... hopefully, will soon start creating the 1st version for downloading videos in cli soon.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@
 # |                                                      |
 # + ---------------------------------------------------- +
 
+# Busy days due to work and college starting at the same time... will update the work soon
+# most of the stuff are in testing phase and might also include config files incase I couldnt solve the laying issue.
+# Found a guide on creating the gui using python as I donot have much knowledge in python
+# However, the first version of this app will be in CLI-CMD mode while working on gui in seperate file which will only be uploaded once finished and functions as desired.
+# The github is just for placeholder rn... will be working locally before publishing a working version independent of my own device
+# Sadly this app is only gonna be for windows as I donot own any mac-devices. My apologies for all the macOS users.
+
+
 # import .config
 import sys
 import json
